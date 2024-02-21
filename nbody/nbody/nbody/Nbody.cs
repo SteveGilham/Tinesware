bc795aa51f6e2e4200737577754ea5aeb3efddca
SteveGilham <SteveGilham@users.noreply.github.com>
2024-02-21 11:09:36 +0000

    All the easy bits done

--- nbody/nbody/nbody/Nbody.cs
+++ nbody/nbody/nbody/Nbody.cs
@@ -1,14 +1,10 @@
 //import package UK.co.demon.windsong.tines.?
 
-import java.awt.*;
-import java.applet.*;
-import java.io.*;
-
-/** 
+/**
 *  Class Nbody
 *
 *  This is a simple Nbody iterator; with the demonstration
-*  case being derived from the system of lambda Serpentis and 
+*  case being derived from the system of lambda Serpentis and
 *  its moons from the Blue Planet RPG
 *
 *  It is implemented as a stand-alone Application, which has been
@@ -22,191 +18,203 @@ import java.io.*;
 *
 */
 
-public class Nbody extends Applet implements Runnable, Actor
+namespace nbody
 {
-   Thread appletDynamic;
-   Animation engine;
-   NbodyAnimation ac;
-   Frame display = null;
-   boolean master = false;
-   ActionButton b;
-   Scrollbar sb;
-   Label l;
+  public class Nbody //extends Applet implements Runnable, Actor
 
-   public boolean handleEvent(Event evt)
-   {
-      if(ac.begun) return super.handleEvent(evt);
-      if(evt.target == sb)
+  {
+    //Thread appletDynamic;
+    //Animation engine;
+    private NbodyAnimation ac;
+
+    //Frame display = null;
+    private bool master = false;
+
+    //ActionButton b;
+    //Scrollbar sb;
+    //Label l;
+
+#if TODO
+    public boolean handleEvent(Event evt)
+    {
+      if (ac.begun) return super.handleEvent(evt);
+      if (evt.target == sb)
       {
-         double d = ((double)sb.getValue())*0.1;
-         l.setText(""+d);
-         ac.theta = d*Math.PI/180.0;
-         return true;
+        double d = ((double)sb.getValue()) * 0.1;
+        l.setText("" + d);
+        ac.theta = d * Math.PI / 180.0;
+        return true;
       }
       return super.handleEvent(evt);
-  }
+    }
 
-   /**
-   * Main initialization
-   * @see Applet#init
-   */
-   public void init()
-   {
+    /**
+    * Main initialization
+    * @see Applet#init
+    */
+
+    public void init()
+    {
       ac = new NbodyAnimation();
       engine = new Animation(ac);
 
-      setLayout(new GridLayout(1,1));
+      setLayout(new GridLayout(1, 1));
 
-      if(master)
+      if (master)
       {
-         display = new Frame("Lambda Serpentis II");
+        display = new Frame("Lambda Serpentis II");
       }
       else
       {
-         display = new BasicFrame("Lambda Serpentis II");
-         ((BasicFrame)display).setText(getAppInfo());
-         ((BasicFrame)display).makeSlave();
+        display = new BasicFrame("Lambda Serpentis II");
+        ((BasicFrame)display).setText(getAppInfo());
+        ((BasicFrame)display).makeSlave();
       }
       Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-      display.resize(d.width/2, d.height/2);
+      display.resize(d.width / 2, d.height / 2);
       display.show();
 
       // needed for java in JDK 1.1
-      engine.resize(d.width/2, d.height/2);
+      engine.resize(d.width / 2, d.height / 2);
 
-      display.setLayout(new GridLayout(1,1));
+      display.setLayout(new GridLayout(1, 1));
       display.add(engine);
 
       setLayout(new BorderLayout());
 
       Panel ps = new Panel();
       sb = new Scrollbar(Scrollbar.HORIZONTAL, 0, 50, 0, 3600);
-      add("Center",ps);
+      add("Center", ps);
       ps.setLayout(new BorderLayout());
       ps.add("South", sb);
 
       l = new Label("0.0");
-      ps.add("North",l);
+      ps.add("North", l);
       add("North", new Label("Separation of the moons (degrees)"));
 
       Panel pb = new Panel();
       b = new ActionButton("Start", this);
-      add("South",pb);
+      add("South", pb);
       pb.add(b);
-   }
+    }
 
-
-   public void doIt(Component bb)
-   {
-      if(ac.begun)
+    public void doIt(Component bb)
+    {
+      if (ac.begun)
       {
-         ac.reset();
-         b.setLabel("Start");
+        ac.reset();
+        b.setLabel("Start");
       }
       else
       {
-         ac.begin();
-         b.setLabel("Stop");
-         b.invalidate();
+        ac.begin();
+        b.setLabel("Stop");
+        b.invalidate();
       }
-   }
+    }
 
+    /**
+    * Main (re-)start; thread is
+    * fired up at this point.
+    */
 
-
-   /**
-   * Main (re-)start; thread is
-   * fired up at this point.
-   */
-   public void start()
-   {
-      if(display != null) display.show();
-      if(null == appletDynamic)
+    public void start()
+    {
+      if (display != null) display.show();
+      if (null == appletDynamic)
       {
-         appletDynamic = new Thread(this);
-         appletDynamic.start();
+        appletDynamic = new Thread(this);
+        appletDynamic.start();
       }
-   }
+    }
 
-   /**
-   * Main graceful suppression (iconise,
-   * leave page or whatever).  Called before
-   * destroy()
-   */
-   public void stop()
-   {
-      if(display != null) display.hide();
-      if(null != appletDynamic)
+    /**
+    * Main graceful suppression (iconise,
+    * leave page or whatever).  Called before
+    * destroy()
+    */
+
+    public void stop()
+    {
+      if (display != null) display.hide();
+      if (null != appletDynamic)
       {
-         //appletDynamic.stop();
-         appletDynamic = null;
+        //appletDynamic.stop();
+        appletDynamic = null;
       }
-   }
+    }
 
-   /**
-   * Main final termination and tidy
-   */
-   public void destroy()
-   {
-   }
+    /**
+    * Main final termination and tidy
+    */
 
-   /**
-   * Output to screen
-   * @param g Graphic to which to draw
-   */
-   public void paint(Graphics g)
-   {
+    public void destroy()
+    {
+    }
+
+    /**
+    * Output to screen
+    * @param g Graphic to which to draw
+    */
+
+    public void paint(Graphics g)
+    {
       engine.repaint();
-   }
+    }
 
-   /**
-   * work routine
-   */
-   public void run()
-   {
-      for(;;)
+    /**
+    * work routine
+    */
+
+    public void run()
+    {
+      for (; ; )
       {
-         repaint();
-         try{ Thread.sleep(100);}
-         catch(InterruptedException ignored){}
+        repaint();
+        try { Thread.sleep(100); }
+        catch (InterruptedException ignored) { }
       }
-   }
+    }
 
-   public String getAppletInfo()
-   {
-       return Nbody.getAppInfo();
-   }
+    public String getAppletInfo()
+    {
+      return Nbody.getAppInfo();
+    }
 
-   /**
-   * Return license etc details
-   */
-   public static String getAppInfo()
-   {
+    /**
+    * Return license etc details
+    */
+
+    public static String getAppInfo()
+    {
       return
        "Nbody - An N-body simulation\r\n"
-      +"Version 1.1 Coded & copyright Mr. Tines <tines@windsong.demon.co.uk> 1997-2002\r\n"
-      +"This program is an implementation of Sverre Aarseth's N-body code\r\n"
-      +"as given in Binney & Tremaine's _Galactic Dynamics, and converted from\r\n"
-      +"the original Fortran.  The demonstration case is the planet Lambda Serpentis II\r\n"
-      +"and its moons, as per the RPG _Blue Planet (and that is me in the\r\n"
-      +"Special Thanks section in the 1st edition of that game)\r\n\r\n"
-      +Main.GNU_General_Public_Licence();
-   }
+      + "Version 1.1 Coded & copyright Mr. Tines <tines@windsong.demon.co.uk> 1997-2002\r\n"
+      + "This program is an implementation of Sverre Aarseth's N-body code\r\n"
+      + "as given in Binney & Tremaine's _Galactic Dynamics, and converted from\r\n"
+      + "the original Fortran.  The demonstration case is the planet Lambda Serpentis II\r\n"
+      + "and its moons, as per the RPG _Blue Planet (and that is me in the\r\n"
+      + "Special Thanks section in the 1st edition of that game)\r\n\r\n"
+      + Main.GNU_General_Public_Licence();
+    }
 
-   /**
-   * Return parameter details
-   * @see Applet.getParameterInfo
-   */
-   public String[][] getParameterInfo()
-   {
-      String[][]t = {{"None"},{"N/A"},{"This applet is purely GUI driven"}};
+    /**
+    * Return parameter details
+    * @see Applet.getParameterInfo
+    */
+
+    public String[][] getParameterInfo()
+    {
+      String[][] t = { { "None" }, { "N/A" }, { "This applet is purely GUI driven" } };
       return t;
-   }
+    }
 
-   /**
-   * Applicationizer function
-   */
-   public static void main(String [] args)
-   {
+    /**
+    * Applicationizer function
+    */
+
+    public static void main(String[] args)
+    {
       Nbody self = new Nbody();
       self.master = true;
       BasicFrame window = new BasicFrame("Nbody");
@@ -218,14 +226,15 @@ public class Nbody extends Applet implements Runnable, Actor
       self.init();
       Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 
-      window.resize(250,150);
+      window.resize(250, 150);
 
       window.show();
       self.run();
       self.stop();
       self.destroy();
-   }
+    }
+#endif
+  }
+
+  /* end of file Nbody.java */
 }
-
-/* end of file Nbody.java */
-
